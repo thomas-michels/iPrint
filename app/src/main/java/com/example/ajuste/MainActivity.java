7669edd7c6440036ff613b1c19346fa8d58246a7
thomas-rodrigues <thomas.rodrigues@hbsis.com.br>
2020-10-08 17:50:02 -0300

    Criando repositorio

--- app/src/main/java/com/example/ajuste/MainActivity.java
+++ app/src/main/java/com/example/ajuste/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
 
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
