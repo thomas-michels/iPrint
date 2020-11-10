2ebde5b30f256f8eba396e10a838e4da9cf4508c
thomas-rodrigues <thomas.rodrigues@hbsis.com.br>
2020-11-10 17:53:46 -0300

    pagina de producao pronta

--- app/src/main/java/com/example/ajuste/MainActivity.java
+++ app/src/main/java/com/example/ajuste/MainActivity.java
@@ -2,17 +2,10 @@ package com.example.ajuste;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 import com.example.ajuste.ui.main.SectionsPagerAdapter;
 
